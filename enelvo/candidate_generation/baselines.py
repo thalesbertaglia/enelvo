c8f0aca831b24611330ea2f5b9876012a0c7c49e
tfcbertaglia <tfcbertaglia@gmail.com>
2017-06-03 18:17:22 -0300

    Candidate generation now returns the noisy word as well

--- enelvo/candidate_generation/baselines.py
+++ enelvo/candidate_generation/baselines.py
@@ -5,7 +5,7 @@
 from enelvo import metrics
 
 
-def generation_similarity_metric(lex, word, metric=metrics.edit_distance, threshold=2, geq=False, n_cands=-1):
+def generate_by_similarity_metric(lex, word, metric=metrics.edit_distance, threshold=2, geq=False, n_cands=-1):
     '''A simple similarity metric baseline for candidate generation.
 
     All canonical words with ``metric`` below ``threshold`` (or above, if ``geq`` is set to True) are considered
@@ -18,15 +18,15 @@ def generation_similarity_metric(lex, word, metric=metrics.edit_distance, thresh
         metric (function): The similarity metric.
         threshold (float): Maximum metric value between words to consider them as candidates.
         geq (boolean): Whether the metric must be below or above ``threshold``. If ``geq`` is True, then the threshold is inverted.
-        n_cands (int): Number of candidates to be returned (i.e, top ``n_cands`` will be returned according to their frequencies).
+        n_cands (int): Number of candidates to be returned (i.e, top ``n_cands`` will be returned in alphabetical order).
                        By default, all candidates (-1) that satisfy the metric threshold are returned.
 
     Returns:
-        list: A list containing ``n_cands`` normalization candidates.
+        tuple(str, list(str)): The noisy word and a list containing ``n_cands`` normalization candidates.
     '''
     # Comparison function changes according to ``geq`` flag.
     comp = lambda x, y: x >= y if geq else x <= y
 
     candidates = sorted([c for c in lex if comp(metric(word, c), threshold)])
 
-    return candidates if n_cands == -1 else candidates[:n_cands]
+    return (word,candidates) if n_cands == -1 else (word,candidates[:n_cands])
