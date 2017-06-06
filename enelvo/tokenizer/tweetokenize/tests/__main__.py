3a5847372f139b6bee8fe343caa6f3704b32763c
tfcbertaglia <tfcbertaglia@gmail.com>
2017-06-06 19:19:16 -0300

    Reorganized preprocessing related files

--- enelvo/tokenizer/tweetokenize/tests/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import unittest
-from test_tweetokenize import TokenizeTests
-
-suite = unittest.TestSuite()
-suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TokenizeTests))
-
-unittest.TextTestRunner().run(suite)
