bc8820a3863267a80af738ccb0c2fd99b1a05a39
tfcbertaglia <tfcbertaglia@gmail.com>
2017-12-12 23:31:14 +0000

    Updated version

--- enelvo/__init__.py
+++ enelvo/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'Enelvo'
 __summary__ = 'A flexible normalizer for user-generated content.'
 __uri__ = 'https://www.github.com/tfcbertaglia/enelvo'
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __author__ = 'Thales Bertaglia'
 __email__ = 'thalesbertaglia@gmail.com'
