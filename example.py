40f38206be6efabe039db566b6bc051dcf65d888
tfcbertaglia <tfcbertaglia@gmail.com>
2018-01-10 10:33:09 +0000

    Created example

---
+++ example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''A simple example showing how to integrate Enelvo to your code, using it as a package.'''
+
+# Author: Thales Bertaglia <thalesbertaglia@gmail.com>
+
+# normaliser contains the Normaliser class
+from enelvo import normaliser
+
+
+def main():
+    # Let's use this sentence as example
+    sentence = 'hj eh q dia vc sbe?'
+    # Creates a normaliser object with default attributes.
+    norm = normaliser.Normaliser()
+    # Now, let's normalise the original sentence
+    norm_sentence = norm.normalise(sentence)
+    # Simple! Let's see the result
+    print(norm_sentence)
+    # We can change any attributes we want
+    norm.capitalize_inis = True
+    # Let's see the result
+    print(norm.normalise(sentence))
+    # That's all!
+
+if __name__ == '__main__': main()
