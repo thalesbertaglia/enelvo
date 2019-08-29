cf191d0a27db578d293e69047cd584c2d41d3afe
tfcbertaglia <tfcbertaglia@gmail.com>
2019-08-29 22:48:15 +0200

    Add test for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -40,3 +40,31 @@ def test_diacritic_sym():
     assert metrics.diacritic_sym("ímã", "íma") == 1
     assert metrics.diacritic_sym("vose", "você") == 1
     assert metrics.diacritic_sym("maca maçã ãôíá", "abc") == 0
+
+
+def test_lcs_ratio_sym():
+    assert metrics.lcs_ratio_sym("maca", "maçã") == 1.0
+    assert metrics.lcs_ratio_sym("íimmãoaea", "iimmãó") == 6 / 9
+    assert metrics.lcs_ratio_sym("íimmãoaea", "zwwqsdd") == 0.0
+
+
+def test_hassan_similarity():
+    assert metrics.hassan_similarity("casa", "caza") == 0.75
+    assert metrics.hassan_similarity("qq eh isso", "o que é isso") == (7 / 12) / 5
+    assert metrics.hassan_similarity("abc def zzz", "abc def zzz") == 1.0
+
+
+def test_c_hassan_similarity():
+    assert metrics.c_hassan_similarity("casa", "caza") == 0.25
+    assert metrics.c_hassan_similarity("qq eh isso", "o que é isso") == 1 - (
+        (7 / 12) / 5
+    )
+    assert metrics.c_hassan_similarity("abc def zzz", "abc def zzz") == 0.0
+
+
+def test_word_frequency():
+    lex = {"a": 5, "b": 15, "c": 2}
+    assert metrics.word_frequency(lex, "a") == 5
+    assert metrics.word_frequency(lex, "b") == 15
+    assert metrics.word_frequency(lex, "c") == 2
+    assert metrics.word_frequency(lex, "d") == 0
