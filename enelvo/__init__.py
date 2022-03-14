34c8d38931e0fd1401cb656d74868df90591e824
Thales Bertaglia <thales.bertaglia@gmail.com>
2022-03-14 20:27:25 +0100

    Update __init__.py

--- enelvo/__init__.py
+++ enelvo/__init__.py
@@ -3,7 +3,7 @@
 Enelvo
 ~~~~~~~~~~~~~~~~~~~
 A flexible normalizer for user-generated content.
-:copyright: (c) 2017-2019 by Thales Bertaglia
+:copyright: (c) 2017-2022 by Thales Bertaglia
 :licence: MIT, see LICENSE for more details
 """
 import logging
@@ -11,12 +11,12 @@ import logging
 __prog__ = "enelvo"
 __title__ = "Enelvo"
 __summary__ = "A flexible normaliser for user-generated content."
-__uri__ = "https://www.github.com/tfcbertaglia/enelvo"
+__uri__ = "https://www.github.com/thalesbertaglia/enelvo"
 __author__ = "Thales Bertaglia"
 __email__ = "contact@thalesbertaglia.com"
 
 __license__ = "MIT"
-__copyright__ = "Copyright 2017-2020 Thales Bertaglia"
+__copyright__ = "Copyright 2017-2022 Thales Bertaglia"
 
 # the user should dictate what happens when a logging event occurs
 logging.getLogger(__name__).addHandler(logging.NullHandler())
